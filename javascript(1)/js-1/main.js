91c7367fe44a3ad342c2211aa39b9b6632c0ed77
BluesKai <94417614+Kennen030@users.noreply.github.com>
2023-03-23 14:36:45 +0800

    Add files via upload

---
+++ javascript(1)/js-1/main.js
@@ -0,0 +1,20 @@
+// 运行弹窗提示
+alert('pokemon');
+
+// 从脚本中向控制台输出
+console.log('pokemon');
+
+// 向控制台输出警告
+// console.warn('警告');
+
+// 向控制台输出信息
+// console.info('信息');
+
+// 三种声明方式:var,let,const
+// var是全局作用域
+
+// let可以重新赋值,const不可以会报错
+let age = 30;
+age = 31;
+
+console.log(age);
